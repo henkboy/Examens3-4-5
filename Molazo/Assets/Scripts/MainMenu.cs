bbdfe82ffa7cf2003d7ae0f14aaa37c1724a0a07
henkboy <henkboy99@gmail.com>
2019-01-25 12:02:43 +0100

    lvl 2 added, menu + script

---
+++ Molazo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject MenuPanel;
+    public GameObject PlayPanel;
+
+    public void Play()
+    {
+        MenuPanel.SetActive(false);
+        PlayPanel.SetActive(true);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Continue()
+    {
+
+    }
+
+    public void Back()
+    {
+        PlayPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
--- Molazo/Assets/Scripts/Reset.cs
+++ Molazo/Assets/Scripts/Reset.cs
@@ -6,13 +6,19 @@ public class Reset : MonoBehaviour {
 
     public GameObject Player;
     public Player playerscript;
+
     // Level 1
     public GameObject DoubleJumpPowerup;
-    public GameObject Blockade;
+    public GameObject BlockadeLvl1;
+
+    // Level 2
+    public GameObject Blockade1Lvl2;
+    public GameObject Blockade2Lvl2;
+
 
     void OnTriggerEnter(Collider Col)
     {
-        if(gameObject.name == "FloorLvl1")
+        if(0 == 0)
         {
             ResetLvl1();
         }
@@ -21,15 +27,24 @@ public class Reset : MonoBehaviour {
     void ResetLvl1()
     {
         Player.transform.position = new Vector3(0, 30, -6);
-        //Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
-        //Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
         playerscript.rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
         playerscript.rb.constraints = RigidbodyConstraints.FreezeRotation;
         playerscript.HasWeapon = false;
         playerscript.DoubleJump = false;
         playerscript.Health = 100;
         DoubleJumpPowerup.SetActive(true);
-        Blockade.SetActive(true);
-        
+        BlockadeLvl1.SetActive(true); 
+    }
+
+    void ResetLvl2()
+    {
+        Player.transform.position = new Vector3(-41, 25, -60);
+        playerscript.rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        playerscript.rb.constraints = RigidbodyConstraints.FreezeRotation;
+        playerscript.HasWeapon = false;
+        playerscript.DoubleJump = false;
+        playerscript.Health = 100;
+        Blockade1Lvl2.SetActive(true);
+        Blockade2Lvl2.SetActive(true);
     }
 }
